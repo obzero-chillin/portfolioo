9360c346a41721c1da7ead7f68ad992d96dcb9a8
Obonolo Modise <obonolomodd@gmail.com>
2025-12-27 16:36:41 +0200

    added fcc png instead of pdf

--- docs/src/components/home.jsx
+++ docs/src/components/home.jsx
@@ -20,7 +20,7 @@ const Homie = () => {
                 I'm passionate about building efficient and scalable web applications, programmes that solve real-world problems.
             </h3>
 
-            <div className="cert" role="button" onClick={()=>{window.open(URL='./src/assets/fcc.pdf', '_blank')}}>
+            <div className="cert" role="button" onClick={()=>{window.open(URL='./src/assets/fcc.png', '_blank')}}>
                 FreeCodeCamp Certification
                 <div className="certArrCont">
 
